dbc345fd657723927d32f507476047c5dd77a8a2
abdobeh <abdobeh@live.com>
2022-02-01 15:27:31 +0100

    React Setup

---
+++ stripe-react/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+export const stripePromise = loadStripe(
+  'pk_test_51KOLX7Cr1L2XVSYVZGooqLBgrxxcRIs45KW0WyynRMvHgBtPJmuzaVl81tquWdRf5xdjaYqLxj26SPSWjWd5Ajyi00fIbxWTlk'
+)
+ReactDOM.render(
+  <React.StrictMode>
+    <Elements stripe = {stripePromise}>
+    <App />
+    </Elements>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
